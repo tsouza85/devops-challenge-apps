0da9edf5b0eeb0bf385937e6e24cedc446659e43
Tiago Souza <43278620+tsouza85@users.noreply.github.com>
2019-04-23 15:19:58 -0300

    create deploy module

---
+++ iac/modules/deploy/api/main.tf
@@ -0,0 +1,128 @@
+resource "kubernetes_namespace" "devops-challenge-api-ns" {
+  metadata {
+    annotations {
+      name = "devops-challenge-api-stack"
+    }
+
+    name = "devops-challenge-api-stack"
+  }
+}
+
+resource "kubernetes_secret" "devops-challenge-api-secret" {
+  metadata {
+    name = "devops-challenge-api-secret"
+    namespace = "${kubernetes_namespace.devops-challenge-api-ns.metadata.0.name}"
+  }
+
+  data {
+    DB = "${var.database_connection_string}"
+  }
+}
+
+resource "kubernetes_config_map" "devops-challenge-api-config" {
+  metadata {
+    name = "devops-challenge-api-config"
+    namespace = "${kubernetes_namespace.devops-challenge-api-ns.metadata.0.name}"
+  }
+
+  data {
+    PORT     = "${var.api_port}"
+  }
+}
+
+resource "kubernetes_deployment" "devops-challenge-api-app" {
+  "metadata" {
+    name = "devops-challenge-api-app"
+    namespace = "${kubernetes_namespace.devops-challenge-api-ns.metadata.0.name}"
+    labels {
+      app = "api"
+    }
+  }
+
+  "spec" {
+    replicas = 3
+
+    selector {
+      match_labels {
+        app = "api"
+      }
+    }
+
+    "template" {
+      "metadata" {
+        name = "devops-challenge-api-app"
+        namespace = "${kubernetes_namespace.devops-challenge-api-ns.metadata.0.name}"
+        labels {
+          app = "api"
+        }
+      }     
+      spec { 
+        container {
+          name = "api"
+          image = "tsouza85/devops-challenge-api:latest"
+          env_from{
+            config_map_ref{ name = "${kubernetes_config_map.devops-challenge-api-config.metadata.0.name}" }
+          }
+          env_from{
+            secret_ref{ name = "${kubernetes_secret.devops-challenge-api-secret.metadata.0.name}" }
+          }          
+          resources{
+            limits{
+              cpu    = "0.1"
+              memory = "128Mi"
+            }
+            requests{
+              cpu    = "100m"
+              memory = "16Mi"
+            }
+          }
+          port {
+            container_port = "${var.api_port}"
+          }
+          liveness_probe{
+            http_get {
+              path = "/api/status" 
+              port = "${var.api_port}"
+            } 
+            success_threshold = 1
+            failure_threshold = 3
+            period_seconds = 20
+            timeout_seconds = 10
+          }
+          readiness_probe{
+            http_get {
+              path = "/api/status" 
+              port = "${var.api_port}"
+            } 
+            success_threshold = 1
+            failure_threshold = 3
+            period_seconds = 20
+            timeout_seconds = 10
+          }          
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "devops-challenge-api-service" {
+  "metadata" {
+    name = "devops-challenge-api-app"
+    namespace = "${kubernetes_namespace.devops-challenge-api-ns.metadata.0.name}"
+    labels {
+      app = "api"
+    }
+  }
+  spec {
+    selector {
+      app = "${kubernetes_deployment.devops-challenge-api-app.metadata.0.labels.app}"
+    }
+    session_affinity = "ClientIP"
+    port {
+      port = "${var.api_port}"
+      target_port = "${var.api_port}"
+    }
+
+    type = "LoadBalancer"
+  }
+}
---
+++ iac/modules/deploy/web/main.tf
@@ -0,0 +1,116 @@
+resource "kubernetes_namespace" "devops-challenge-web-ns" {
+  metadata {
+    annotations {
+      name = "devops-challenge-web-stack"
+    }
+
+    name = "devops-challenge-web-stack"
+  }
+}
+
+resource "kubernetes_config_map" "devops-challenge-web-config" {
+  metadata {
+    name = "devops-challenge-web-config"
+    namespace = "${kubernetes_namespace.devops-challenge-web-ns.metadata.0.name}"
+  }
+
+  data {
+    PORT     = "${var.app_port}"
+    API_HOST = "${var.api_host}"
+  }
+}
+
+resource "kubernetes_deployment" "devops-challenge-web-app" {
+  "metadata" {
+    name = "devops-challenge-web-app"
+    namespace = "${kubernetes_namespace.devops-challenge-web-ns.metadata.0.name}"
+    labels {
+      app = "web"
+    }
+  }
+
+  "spec" {
+    replicas = 3
+
+    selector {
+      match_labels {
+        app = "web"
+      }
+    }
+
+    "template" {
+      "metadata" {
+        name = "devops-challenge-web-app"
+        namespace = "${kubernetes_namespace.devops-challenge-web-ns.metadata.0.name}"
+        labels {
+          app = "web"
+        }
+      }     
+      spec { 
+        container {
+          name = "web"
+          image = "tsouza85/devops-challenge-web:latest"
+          env_from{
+            config_map_ref{ name = "${kubernetes_config_map.devops-challenge-web-config.metadata.0.name}" }
+          }
+          resources{
+            limits{
+              cpu    = "0.1"
+              memory = "128Mi"
+            }
+            requests{
+              cpu    = "100m"
+              memory = "16Mi"
+            }
+          }
+          port {
+            container_port = "${var.app_port}"
+          }
+
+          liveness_probe{
+            http_get {
+              path = "/" 
+              port="${var.app_port}"
+            } 
+            success_threshold = 1
+            failure_threshold = 3
+            period_seconds = 20
+            timeout_seconds = 10
+          }
+          readiness_probe{
+            http_get {
+              path = "/" 
+              port="${var.app_port}"
+            } 
+            success_threshold = 1
+            failure_threshold = 3
+            period_seconds = 20
+            timeout_seconds = 10
+          }          
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "devops-challenge-web-service" {
+  "metadata" {
+    name = "devops-challenge-web-app"
+    namespace = "${kubernetes_namespace.devops-challenge-web-ns.metadata.0.name}"
+    labels {
+      app = "web"
+    }
+  }
+  spec {
+    selector {
+      app = "${kubernetes_deployment.devops-challenge-web-app.metadata.0.labels.app}"
+    }
+    session_affinity = "ClientIP"
+    port {
+      port = "${var.app_port}"
+      target_port = "${var.app_port}"
+    }
+
+    type = "LoadBalancer"
+  }
+}
